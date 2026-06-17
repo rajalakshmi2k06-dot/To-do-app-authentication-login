a90f300a9a7bbd4b81a80b96632f80079d481c9a
rajalakshmi2k06-dot <rajalakshmi2k06@gmail.com>
2026-06-17 15:45:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -172,6 +172,6 @@ def toggle_status(todo_id):
     return jsonify({"success": True, "message": "Status updated!"})
 
 
-if __name__ == "__main__":
-    print("🚀 Server running at http://localhost:5000")
-    app.run(debug=True, port=5000)
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=False, host="0.0.0.0", port=port)
